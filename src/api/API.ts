edfe3f8ec5ca50b3ae34d2b2cf29ebe36daeac1f
Sam Leifer <samleifer@Sams-Air.lan>
2025-01-12 13:42:33 -0600

    env

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export abstract class API {
-    rootPath: string = "http://localhost:8000"
+    rootPath: string = import.meta.env.VITE_BACKEND_API_ROOT
     subPath: string = ""
 
     constructor(subPath: string) {
