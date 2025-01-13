c5b524c578fa9acc1a5acb21e77a38dc87d6ee36
Sam Leifer <samleifer@Sams-Air.lan>
2025-01-13 17:04:53 -0600

    cors

--- src/api/API.ts
+++ src/api/API.ts
@@ -28,8 +28,11 @@ export abstract class API {
         return axios
             .get(path, {
                 "headers": {
+                    "Content-Type": "application/json",
                     "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json"
+                    "Access-Control-Allow-Credentials":  "true",
+                    "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST, PUT",
+                    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
                 }
             })
             .then(res => res)
