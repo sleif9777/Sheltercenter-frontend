12dd22454f9232a7dffea8279273bff166f8b55e
Sam Leifer <samleifer@Sams-Air.lan>
2025-01-23 20:36:54 -0600

    More redundancies

--- src/components/card/CardItem.tsx
+++ src/components/card/CardItem.tsx
@@ -1,10 +1,5 @@
 import { TwoColumnListItem } from "../two_column_list/TwoColumnList"
 
-// export interface CardItemProps extends TwoColumnListItem {
-//     link?: string, // Mutually exclusive with modal
-//     modal?: JSX.Element, // Mutually exclusive with link
-// }
-
 export function CardItem(props: TwoColumnListItem) {
     const { link, text, onClick } = props
 
