461eabccbbe579229b7f461615f61521c5f50fa2
Sam Leifer <samleifer@Sams-Air.lan>
2025-01-26 15:31:05 -0600

    login fixes

--- src/apps/login/LoginApp.tsx
+++ src/apps/login/LoginApp.tsx
@@ -15,10 +15,26 @@ export function LoginApp() {
     const [email, setEmail] = useState<string>("")
     const [OTP, setOTP] = useState<string>("")
     const [otpGenerated, setOTPGenerated] = useState<boolean | null>(null)
+    const [generatingOTP, setGeneratingOTP] = useState<boolean>(false)
     const [messageLevel, setMessageLevel] = useState<MessageLevel>()
     const [message, setMessage] = useState<string>("")
     const api = new UserProfilesAPI()
     const store = useStore(useSessionState)
+
+    const generateOTPTrigger = async () => {
+        setGeneratingOTP(true)
+        const intraAuthContext: AxiosResponse<{ message?: string }> = await api.GenerateOTP(email)
+        setOTPGenerated(intraAuthContext.status === 202)
+        setMessageLevel(intraAuthContext.status === 202 ? "Success" : "Error")
+        setMessage(intraAuthContext.data.message ?? "")
+        setGeneratingOTP(false)
+    }
+
+    const attemptLogInTrigger = async () => {
+        await store.attemptLogIn(email, OTP)
+        setMessageLevel(store.message ? "Error" : "Default")
+        setMessage(store.message ?? "")
+    }
 
     return <div id="page-container">
         <img 
@@ -37,6 +53,11 @@ export function LoginApp() {
                 value={email}
                 error={email.length > 0 && !EmailValidator.validate(email)}
                 onChange={(e) => setEmail(e.target.value.toLocaleLowerCase())}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter" && !generatingOTP) {
+                        generateOTPTrigger()
+                    }
+                }}
             />
         </div>
         {
@@ -51,23 +72,29 @@ export function LoginApp() {
                             value={OTP}
                             error={OTP.length > 0 && OTP.length !== 8}
                             onChange={(e) => setOTP(e.target.value.toLocaleUpperCase())}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                    attemptLogInTrigger()
+                                }
+                            }}
                         />
                     </div>
                     <div>
                         <SubmissionButton 
                             disabled={OTP.length > 0 && OTP.length !== 8} 
-                            extendOnSubmit={async () => {
-                                await store.attemptLogIn(email, OTP)
-                                setMessageLevel(store.message ? "Error" : "Default")
-                                setMessage(store.message ?? "")
-                            }}
+                            // extendOnSubmit={async () => {
+                            //     await store.attemptLogIn(email, OTP)
+                            //     setMessageLevel(store.message ? "Error" : "Default")
+                            //     setMessage(store.message ?? "")
+                            // }}
+                            extendOnSubmit={() => attemptLogInTrigger()}
                             textOverride="Log In"
                         />
                     </div>
                 </>
                 : <div>
                     <SubmissionButton 
-                        disabled={email.length == 0 || !EmailValidator.validate(email)} 
+                        disabled={email.length == 0 || !EmailValidator.validate(email) || generatingOTP} 
                         extendOnSubmit={async () => {
                             const intraAuthContext: AxiosResponse<{ message?: string }> = await api.GenerateOTP(email)
                             setOTPGenerated(intraAuthContext.status === 202)
