de178f8281e4866cbed18e72a1736de287802ef2
Sam Leifer <samleifer@Sams-Air.lan>
2026-03-30 23:43:34 -0500

    fix: add allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react"
 import { config } from "dotenv"
 import { defineConfig } from "vite"
 
-// Load environment variables from .env file
 config()
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwind()],
+	server: {
+		allowedHosts: ["savinggracencscheduler.com"],
+	},
 })
